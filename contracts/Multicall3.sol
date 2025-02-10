9999527e48383eec2eae86b7c8af2f2e102d57db
Vishal <kr.vishal.sh@gmail.com>
2025-02-10 16:09:09 +0530

    Initial Commit

---
+++ contracts/Multicall3.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+
+contract Multicall3 {
+    struct CallData {
+        address target;
+        bytes callData;
+    }
+
+    struct Result {
+        bool success;
+        bytes returnData;
+    }
+
+    function _callCallData(address target, bytes calldata callData)
+        public
+        returns (bool success, bytes memory returnData)
+    {
+        return target.call(callData);
+    }
+
+    function multicall(CallData[] calldata calls) payable external returns (Result[] memory results) {
+        results = new Result[](calls.length);
+        for (uint256 i = 0; i < calls.length; i++) {
+            Result memory result = Result({success: false, returnData: new bytes(0)});
+            (result.success, result.returnData) = _callCallData(calls[i].target, calls[i].callData);
+            require(result.success, "Multicall: call failed");
+            results[i] = result;
+        }
+    }
+}
---
+++ scripts/multicall.ts
@@ -0,0 +1,39 @@
+import hre, { ethers } from "hardhat";
+import fs from "fs";
+
+async function main() {
+	if (!fs.existsSync(`deployments/${hre.network.name}.json`)) {
+		throw new Error(`No deployment file found, run \`npx hardhat run scripts/deploy.ts --network ${hre.network.name}\` first.`);
+	}
+	const { MulticallAddr, TokenAddr, Owner } = JSON.parse(fs.readFileSync(`deployments/${hre.network.name}.json`).toString());
+
+	const [owner, user1, user2, user3] = await ethers.getSigners();
+	const Token = await ethers.getContractAt("ERC20Token", TokenAddr);
+	const Multicall = await ethers.getContractAt("Multicall3", MulticallAddr);
+
+	console.log("🅱️  Before Batch Request: Balance -");
+	console.log("OWNER:", await Token.balanceOf(owner.address));
+	console.log("USER1", await Token.balanceOf(user1.address));
+	console.log("USER2", await Token.balanceOf(user2.address));
+	console.log("USER3", await Token.balanceOf(user3.address));
+	let result = await Token.approve(Multicall.target, 600);
+	await result.wait();
+
+	// Batch Request (multicall)
+	result = await Multicall.multicall([
+		{ target: TokenAddr, callData: Token.interface.encodeFunctionData("transferFrom", [Owner, user1.address, 100]) },
+		{ target: TokenAddr, callData: Token.interface.encodeFunctionData("transferFrom", [Owner, user2.address, 200]) },
+		{ target: TokenAddr, callData: Token.interface.encodeFunctionData("transferFrom", [Owner, user3.address, 300]) },
+	]);
+	await result.wait();
+	console.log("🚀 After Batch Request: Balance -");
+	console.log("OWNER:", await Token.balanceOf(owner.address));
+	console.log("USER1", await Token.balanceOf(user1.address));
+	console.log("USER2", await Token.balanceOf(user2.address));
+	console.log("USER3", await Token.balanceOf(user3.address));
+}
+
+main().catch((error) => {
+	console.error(error);
+	process.exit(1);
+});
